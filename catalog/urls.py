a836cceecc7a3e9f73dac9da3f8b50587e4925c1
Christopher K <christopherfkasuba@gmail.com>
2025-11-09 20:17:38 -0500

    lab finished

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,5 +10,16 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),  # /catalog/authors/
     path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),  # /catalog/author/1/
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
 ]
 
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+# catalog/urls.py (append)
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
